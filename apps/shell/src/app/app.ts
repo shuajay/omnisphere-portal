6ebc8dbadde7770570f44dacebdd73df764aa2c4
ShuaJay <joshuajayphiri5@gmail.com>
2026-05-13 14:44:31 +0200

    feat: sidebar and logo

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Sidebar } from '@omnisphere-portal/ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [
+    NxWelcome, 
+    RouterModule,
+    Sidebar,
+  ],
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected title = 'shell';
+  protected isDarkTheme = false;
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme');
+    this.isDarkTheme = savedTheme === 'dark';
+    document.documentElement.setAttribute(
+      `data-theme`,
+      savedTheme ?? 'light'  
+    );
+  }
+
+  toggleTheme(){
+    this.isDarkTheme = !this.isDarkTheme;
+    const theme = this.isDarkTheme ? 'dark' : 'light';
+    document.documentElement.setAttribute(
+      `data-theme`,
+      theme
+    )
+    localStorage.setItem('theme', theme);
+  }
 }
