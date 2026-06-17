fe1ef35ccf5b23a3d7a1ac7fd6a0043782d66015
ShuaJay <joshuajayphiri5@gmail.com>
2026-06-17 22:59:49 +0200

    dashboard UI

--- apps/shell/src/app/dashboard/dashboard.ts
+++ apps/shell/src/app/dashboard/dashboard.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUsers, faUser, faMedal, faTachographDigital, faBriefcase, faCreditCard, faChartSimple} from '@omnisphere-portal/util'
 
 @Component({
   selector: 'app-dashboard',
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  faUsers = faUsers;
+  faUser = faUser;
+  faMedal = faMedal;
+  faTachographDigital = faTachographDigital;
+  faBriefcase = faBriefcase;
+  faCreditCard = faCreditCard;
+  faChartSimple = faChartSimple;
+}
